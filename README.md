4dfe8dbd5bbd02712d40f5bda358aaec4fabf78f
cipchk <cipchk@gmail.com>
2014-11-23 18:20:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+AngularJS 省份城市联动
+=================
+
+类似淘宝收货地址的所在地。过于仓促，暂时先这样子吧。
+
+
+使用说明
+=================
+
+添加angular.xheditor模块到你的应用。
+
+```javascript
+angular.module('myApp', ['angular.city.select', ...]);
+```
+
+html标记
+```html
+<city-select ng-model="item.city"></city-select>
+```
+
+ng-model必须是一个带有`*.city`对象，另一方面，对于初始化，支持两种方式分别是：
+
+```javascript
+// 按名称顺序，当未找到时中止。
+$scope.item = {
+	city: [ '安徽', '芜湖', '弋江' ]
+};
+
+// 按区域ID
+$scope.item.city = '340203';
+```
+
+bower install
+=================
+
+```bash
+$ bower install angular-city-select --save
+```
---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    less = require('gulp-less'),
+    minifycss = require('gulp-minify-css'),
+    livereload = require('gulp-livereload'),
+    tinylr = require('tiny-lr'),
+    server = tinylr();
+
+var lvr_port = 11852;
+
+var source = {
+	scripts: 'src/angular-city-select.js',
+	less: 'src/angular-city-select.less',
+	demo: 'demo/*.html'
+};
+
+var build = {
+	dir: 'dist'
+};
+
+gulp.task('scripts', function () {
+    return gulp.src(source.scripts)
+        .pipe(uglify())
+        .on("error", handleError)
+        .pipe(gulp.dest(build.dir))
+        .pipe(livereload(server));
+});
+
+gulp.task('styles', function () {
+    return gulp.src(source.less)
+        .pipe(less({
+            paths: [source.less]
+        }))
+        .on("error", handleError)
+        .pipe(minifycss())
+        .pipe(gulp.dest(build.dir))
+        .pipe(livereload(server));
+});
+
+gulp.task('demo', function () {
+    return gulp.src(source.demo)
+        .on("error", handleError)
+        .pipe(gulp.dest(build.dir))
+        .pipe(livereload(server));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function () {
+    try {
+        server.listen(
+          lvr_port,
+          function (err) {
+
+              if (err) { return console.log('1' + err); }
+
+              gulp.watch(source.scripts, ['scripts']);
+              gulp.watch(source.scripts, ['styles']);
+              gulp.watch(source.scripts, ['demo']);
+
+          });
+    }
+    catch (e) {
+        console.log(e);
+    }
+
+});
+
+gulp.task('default', [
+	'scripts',
+	'styles',
+	'demo',
+	'watch'
+]);
+
+
+
+// Error handler
+function handleError(err) {
+    console.log(err.toString());
+    this.emit('end');
+}
